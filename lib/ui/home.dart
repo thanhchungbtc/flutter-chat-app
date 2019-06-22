63021087d2f8ceec5e7776e26454467a0ce49d7a
Thanh Chung Bui <thanhchungbtc@gmail.com>
2019-06-22 11:50:04 +0900

    update home

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,8 +1,8 @@
 import 'package:chat_app_flutter/services/api.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-
   void handleSignOut(BuildContext context) async {
     await Auth.signOut();
     Navigator.of(context).pushReplacementNamed('/');
@@ -11,16 +11,59 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.settings),
-            onPressed: () => handleSignOut(context),
-          )
-        ],
-      ),
-      body: Container(),
-    );
+        appBar: AppBar(
+          title: Text('Home'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () => handleSignOut(context),
+            )
+          ],
+        ),
+        body: StreamBuilder(
+          stream: Firestore.instance.collection('users').snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: Text('No one online yet'),
+              );
+            } else {
+              return ListView.separated(
+                padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 8.0),
+                itemCount: snapshot.data.documents.length,
+                separatorBuilder: (context, index) => Divider(),
+                itemBuilder: (context, index) {
+                  final userData = snapshot.data.documents[index];
+                  return ListTile(
+                    onTap: () {
+                      Navigator.of(context).pushNamed('/chat');
+                    },
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(userData['photoUrl']),
+//                      child: Text(userData['displayName'][0]),
+                    ),
+                    title: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          userData['displayName'],
+                          style: Theme.of(context).textTheme.title,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 4.0),
+                          child: Text(
+                            'Hello',
+                            style: Theme.of(context).textTheme.subtitle,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              );
+            }
+          },
+        ));
   }
 }
