ac4b10f45f0c9379206e27e0ebd8bf78ff5ffd58
Thanh Chung Bui <thanhchungbtc@gmail.com>
2019-06-22 16:50:49 +0900

    can send and receive messages in realtime

--- lib/ui/chat.dart
+++ lib/ui/chat.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ChatScreen extends StatefulWidget {
   @override
@@ -8,15 +11,202 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = TextEditingController();
+
+  String _fromUid, _toUid;
+  String _messageId;
+
+  @override
+  void initState() {
+    super.initState();
+    init();
+  }
+
+  void init() async {
+    final prefs = await SharedPreferences.getInstance();
+    final fromUid = prefs.get('uid');
+    final Map<String, dynamic> args = ModalRoute.of(context).settings.arguments;
+    final toUid = args['toUserId'];
+
+    // construct thread id
+    final CollectionReference messagesRef =
+        Firestore.instance.collection('messages');
+    String messageId = "$fromUid-$toUid";
+    DocumentSnapshot messageSnapshot =
+        await messagesRef.document(messageId).get();
+    if (messageSnapshot == null || !messageSnapshot.exists) {
+      messageId = "$toUid-$fromUid";
+      messageSnapshot = await messagesRef.document(messageId).get();
+      if (messageSnapshot == null && !messageSnapshot.exists) {
+        await messagesRef.document(messageId).setData({
+          'items': [],
+        });
+      }
+    }
+    setState(() {
+      _messageId = messageId;
+    });
+
+    setState(() {
+      _fromUid = fromUid;
+      _toUid = toUid;
+    });
+  }
+
+  void _handleSendMessage(text) {
+    _textController.clear();
+    final message = ChatMessageWidget(
+      text: text,
+      isOutgoingMessage: true,
+    );
+
+    Firestore.instance
+        .collection('messages')
+        .document(_messageId)
+        .collection('items')
+        .document()
+        .setData({
+      'sent_from_id': _fromUid,
+      'content': text,
+      'sent_date': Timestamp.now(),
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final messageList = _messageId != null
+        ? StreamBuilder(
+            stream: Firestore.instance
+                .collection('messages')
+                .document(_messageId)
+                .collection('items')
+                .orderBy('sent_date', descending: true)
+                .snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                final documents = snapshot.data.documents;
+                return ListView.builder(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  itemCount: documents.length,
+                  reverse: true,
+                  itemBuilder: (context, index) {
+                    return ChatMessageWidget(
+                      text: documents[index]['content'],
+                      isOutgoingMessage:
+                          documents[index]['sent_from_id'] == _fromUid,
+                    );
+                  },
+                );
+              } else {
+                return Container();
+              }
+            },
+          )
+        : Container();
+
+    final composeText = IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onChanged: (String text) {},
+                onSubmitted: _handleSendMessage,
+                decoration:
+                    InputDecoration.collapsed(hintText: 'Send a message'),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: Icon(Icons.send),
+                onPressed: () {
+                  _handleSendMessage(_textController.text);
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Chat'),
-      ),
-      body: Center(
-        child: Text('Chat screen'),
-      ),
+        appBar: AppBar(
+          title: Text('Chat'),
+        ),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: messageList,
+              ),
+              Divider(),
+              composeText,
+            ],
+          ),
+        ));
+  }
+}
+
+class ChatMessageWidget extends StatelessWidget {
+  final String text;
+  final bool isOutgoingMessage;
+
+  ChatMessageWidget({this.text, this.isOutgoingMessage});
+
+  @override
+  Widget build(BuildContext context) {
+    var inComingMessage = Row(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(right: 8.0),
+          child: CircleAvatar(
+            radius: 16.0,
+            child: Text(text[0]),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+          child: Text(
+            text,
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            color: Colors.grey[300],
+            borderRadius: BorderRadius.circular(100),
+          ),
+        )
+      ],
+    );
+
+    var outGoingMessage = Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+          child: Text(
+            text,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(100),
+          ),
+        ),
+      ],
+    );
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0),
+      child: isOutgoingMessage ? outGoingMessage : inComingMessage,
     );
   }
 }
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,8 +1,21 @@
 import 'package:chat_app_flutter/services/api.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class HomeScreen extends StatelessWidget {
+class _HomeScreenState extends State<HomeScreen> {
+  String _userId;
+
+  @override
+  void initState() {
+    super.initState();
+    SharedPreferences.getInstance().then((prefs) {
+      setState(() {
+        _userId = prefs.get('uid');
+      });
+    });
+  }
+
   void handleSignOut(BuildContext context) async {
     await Auth.signOut();
     Navigator.of(context).pushReplacementNamed('/');
@@ -10,6 +23,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    if (_userId == null) return Container();
     return Scaffold(
         appBar: AppBar(
           title: Text('Home'),
@@ -28,42 +42,56 @@ class HomeScreen extends StatelessWidget {
                 child: Text('No one online yet'),
               );
             } else {
-              return ListView.separated(
+              return ListView.builder(
                 padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 8.0),
                 itemCount: snapshot.data.documents.length,
-                separatorBuilder: (context, index) => Divider(),
+//                separatorBuilder: (context, index) => Divider(),
                 itemBuilder: (context, index) {
                   final userData = snapshot.data.documents[index];
-                  return ListTile(
-                    onTap: () {
-                      Navigator.of(context).pushNamed('/chat');
-                    },
-                    leading: CircleAvatar(
-                      backgroundImage: NetworkImage(userData['photoUrl']),
+                  if (_userId == userData['uid']) return Container();
+
+                  return Column(
+                    children: <Widget>[
+                      ListTile(
+                        onTap: () {
+                          Navigator.of(context).pushNamed('/chat', arguments: {'toUserId': userData['uid']});
+                        },
+                        leading: CircleAvatar(
+                          backgroundImage: NetworkImage(userData['photoUrl']),
 //                      child: Text(userData['displayName'][0]),
-                    ),
-                    title: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          userData['displayName'],
-                          style: Theme.of(context).textTheme.title,
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 4.0),
-                          child: Text(
-                            'Hello',
-                            style: Theme.of(context).textTheme.subtitle,
-                          ),
+                        title: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              userData['displayName'],
+                              style: Theme.of(context).textTheme.title,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 4.0),
+                              child: Text(
+                                'Hello',
+                                style: Theme.of(context).textTheme.subtitle,
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
+                      ),
+                      Divider(),
+                    ],
                   );
                 },
               );
             }
           },
         ));
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeScreenState();
   }
 }
